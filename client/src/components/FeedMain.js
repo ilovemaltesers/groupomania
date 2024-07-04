9a3919092f954b361b75bbd31ea47ff380b2604e
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-07-04 16:11:45 +0200

    New comment/post comp. To move to seperate file later on

--- client/src/components/FeedMain.js
+++ client/src/components/FeedMain.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
 import Card from "react-bootstrap/Card";
 
 const FeedMainContainer = styled.div`
@@ -11,7 +10,7 @@ const FeedMainContainer = styled.div`
   background-color: #f2f9fc;
 `;
 
-const PostBody = styled(Card)`
+const NewPostBody = styled(Card)`
   width: 100%;
   max-width: 500px;
   margin: 0 20px; /* Ensures some padding on smaller screens */
@@ -20,35 +19,68 @@ const PostBody = styled(Card)`
   min-height: 10em;
 `;
 
-const EmployeeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  background-color: #f0f3f7;
-
-  min-height: 7em;
+const TellMeText = styled.h4`
+  font-size: 1.25em; /* Adjust the size as needed */
 `;
 
-const ImageContainer = styled.div`
+const CommentTextarea = styled.textarea`
+  width: 100%;
+  min-height: 3em;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical; /* Allow vertical resizing */
+`;
+
+const NewCommentButtonContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 10px;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-  background-color: #dfe2e6;
-  min-height: 15em;
+  justify-content: flex-end;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+
+const SubmitNewCommentButton = styled.button`
+  background-color: #bdebff;
+  color: black;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+`;
+
+const UploadImageButton = styled.button`
+  background-color: #bdebff;
+  color: black;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
 `;
 
 const FeedMain = () => {
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
   return (
     <FeedMainContainer>
-      <PostBody>
-        <EmployeeContainer></EmployeeContainer>
-        <ImageContainer></ImageContainer>
-      </PostBody>
+      <NewPostBody>
+        <TellMeText>So what do you want to tell me?</TellMeText>
+        <CommentTextarea
+          placeholder="Write your comment..."
+          value={comment}
+          onChange={handleCommentChange}
+        />
+        {/* Optionally, you can add a submit button or handle form submission */}
+        <NewCommentButtonContainer>
+          <UploadImageButton type="submit">Upload Image</UploadImageButton>
+          <SubmitNewCommentButton type="submit">Submit</SubmitNewCommentButton>
+        </NewCommentButtonContainer>
+      </NewPostBody>
     </FeedMainContainer>
   );
 };
