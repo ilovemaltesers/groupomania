306e4f0d95f293973c8d67496bef72ad68ea9e1a
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-07-08 16:20:23 +0200

    updated auth context hook to include username

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -2,49 +2,45 @@ import React, { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext();
 
-// Custom hook
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState(() => {
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userId");
-    // If token exists, return an object with token and userId, otherwise return null
-    return token ? { token, userId } : null;
+    const userName = localStorage.getItem("userName");
+    return token ? { token, userId, userName } : null;
   });
 
-  // Effect to sync auth state with localStorage values
   useEffect(() => {
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userId");
-    // Update auth state if token or userId changes in localStorage
+    const userName = localStorage.getItem("userName");
     if (
       (token && (!auth || token !== auth.token)) ||
-      (userId && (!auth || userId !== auth.userId))
+      (userId && (!auth || userId !== auth.userId)) ||
+      (userName && (!auth || userName !== auth.userName))
     ) {
-      setAuth({ token, userId });
+      setAuth({ token, userId, userName });
     }
-    // eslint-disable-next-line
-  }, []); // Runs only once on component mount
+  }, []);
 
-  const login = (token, userId) => {
+  const login = (token, userId, userName) => {
     localStorage.setItem("token", token);
     localStorage.setItem("userId", userId);
-
-    setAuth({ token, userId });
+    localStorage.setItem("userName", userName);
+    setAuth({ token, userId, userName });
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userId");
-
+    localStorage.removeItem("userName");
     setAuth(null);
   };
 
-  // Determine if the user is authenticated based on the presence of a token
   const isAuthenticated = !!auth?.token;
 
-  // Render the AuthContext Provider with value containing authentication state and functions
   return (
     <AuthContext.Provider value={{ isAuthenticated, auth, login, logout }}>
       {children}
