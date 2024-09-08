7657b2f27ef7925fdc875f7d819b9ced8b439729
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-09-08 17:36:34 +0200

    likes count matching db on load

--- client/src/components/NewPostComp.js
+++ client/src/components/NewPostComp.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { format } from "date-fns";
 import {
