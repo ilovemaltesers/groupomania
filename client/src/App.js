d95d790e9c38b07cb193e816ed74b505371a7dc7
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-09-13 11:11:23 +0200

    Changed font

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,38 @@ import LoginPage from "./pages/LoginPage";
 import FeedPage from "./pages/FeedPage";
 import ProfilePage from "./pages/ProfilePage";
 import { AuthProvider } from "./contexts/AuthContext";
+import { GlobalStyle } from "./styles/globalStyle";
 // Importing the PrivateRoute component, which protects routes that require authentication.
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/signup" />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/feed"
-          element={
-            <PrivateRoute redirectTo="/login">
-              <FeedPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute redirectTo="/login">
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </AuthProvider>
+    <>
+      <GlobalStyle />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/signup" />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/feed"
+            element={
+              <PrivateRoute redirectTo="/login">
+                <FeedPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute redirectTo="/login">
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </>
   );
 }
 
---
+++ client/src/styles/globalStyle.js
@@ -0,0 +1,18 @@
+// src/GlobalStyle.js
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background-color: #f4f4f4;
+    color: #333;
+  }
+`;
+
+export { GlobalStyle };
